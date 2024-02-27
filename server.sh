f3eb89b74909f53d06d0d792d5aa41e7afae1e48
Mohammad Mohsen <m.m.asemanmanzar@gmail.com>
2024-02-27 10:52:19 +0330

    add USE_RENEW_DELAY_ON_START env: Lego usually adds a [small random delay](https://github.com/go-acme/lego/issues/1656) to the `renew` command, but we don't need this delay at the start because it's not an automated task.

--- server.sh
+++ server.sh
@@ -4,7 +4,11 @@ if [ "$ENABLE_HTTPS" == "true" ]; then
         lego --path $LEGO_PATH $LEGO_NEW_COMMAND_ARGS
     else
         echo "running lego renew command"
-        lego --path $LEGO_PATH $LEGO_RENEW_COMMAND_ARGS
+        no_random_sleep_option="no-random-sleep"
+        if [ "$USE_RENEW_DELAY_ON_START" == "true" ]; then
+            no_random_sleep_option=""
+        fi
+        lego --path $LEGO_PATH $no_random_sleep_option $LEGO_RENEW_COMMAND_ARGS 
     fi
 fi
 
